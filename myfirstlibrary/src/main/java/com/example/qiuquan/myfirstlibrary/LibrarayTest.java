09913035f705b36ba09fd0a48ad5e097295c260f
qq13951870686 <qq58813530@126.com>
2015-12-31 15:29:38 +0800

    第一个library

---
+++ myfirstlibrary/src/main/java/com/example/qiuquan/myfirstlibrary/LibrarayTest.java
@@ -0,0 +1,11 @@
+package com.example.qiuquan.myfirstlibrary;
+
+/**
+ * Created by QIUQUAN on 2015/12/28.
+ */
+public class LibrarayTest {
+
+    public String getTestString(){
+        return "1234567890";
+    }
+}
